351d8088183919e781b9eb33e97d71c629c8e64a
liangfeng <op1637@163.com>
2025-03-16 21:34:44 +0800

    首页修改

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,12 +1,22 @@
 <template>
     <div>
-        <h1> hi, 欢迎来到首页 </h1>
-
+        <h1><span class="username">{{ userStore.username }}</span>, {{ greeting }}好, 欢迎来到首页 </h1>
     </div>
 </template>
 
 <script setup lang="ts">
+import useUserStore from '@/store/modules/user';
+import { getTime } from '@/utils/time';
+
+let userStore = useUserStore();
+
+let greeting = getTime();
 
 </script>
 
-<style scoped></style>
+<style scoped>
+.username {
+    color: yellowgreen;
+    font-weight: bold;
+}
+</style>
